f17c8f481c146ebe6e18a5d5fe87c86e06850c9d
davigabriel15 <davigabrielsilvalima87@gmail.com>
2026-09-20 18:22:40 -0400

    Correção no Multiplayer v2

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAMyxe2W8c05Tr0dR63PNIqOHKBEXSVe4w",
+  authDomain: "pkm-multiplayer.firebaseapp.com",
+  databaseURL: "https://pkm-multiplayer-default-rtdb.firebaseio.com",
+  projectId: "pkm-multiplayer",
+  storageBucket: "pkm-multiplayer.firebasestorage.app",
+  messagingSenderId: "1088629364702",
+  appId: "1:1088629364702:web:9242cefd258cf72922ef4f",
+  measurementId: "G-39JTEX5N8J"
+};
